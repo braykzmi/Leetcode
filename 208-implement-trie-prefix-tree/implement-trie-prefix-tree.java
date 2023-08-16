cd955132b7db9c60867212baeb6cc59bb9deabc3
Brayden Mi <bmi23@sjs.org>
2023-08-16 18:48:20 -0500

    Time: 34 ms (91.91%) | Memory: 54.9 MB (49.43%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,69 @@
+class TrieNode {
+    private TrieNode[] children;
+    private boolean isEndOfWord;
+
+    public TrieNode() {
+        children = new TrieNode[26]; // Assuming lowercase English letters
+        isEndOfWord = false;
+    }
+
+    public boolean containsKey(char ch) {
+        return children[ch - 'a'] != null;
+    }
+
+    public TrieNode get(char ch) {
+        return children[ch - 'a'];
+    }
+
+    public void put(char ch, TrieNode node) {
+        children[ch - 'a'] = node;
+    }
+
+    public void setEndOfWord() {
+        isEndOfWord = true;
+    }
+
+    public boolean isEndOfWord() {
+        return isEndOfWord;
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (!node.containsKey(ch)) {
+                node.put(ch, new TrieNode());
+            }
+            node = node.get(ch);
+        }
+        node.setEndOfWord();
+    }
+
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEndOfWord();
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private TrieNode searchPrefix(String prefix) {
+        TrieNode node = root;
+        for (char ch : prefix.toCharArray()) {
+            if (node.containsKey(ch)) {
+                node = node.get(ch);
+            } else {
+                return null;
+            }
+        }
+        return node;
+    }
+}
