0d51395c3f8064acf1ce561c85ec0b20df022e2c
Brayden Mi <bmi23@sjs.org>
2023-08-16 18:59:49 -0500

    Time: 41 ms (70.03%) | Memory: 43.3 MB (61.43%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[n];
+        int maxLen = 1;
+        
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+        
+        return maxLen;
+    }
+}
